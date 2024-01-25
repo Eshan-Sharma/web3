2a69b5229a13677ced26c0845023c28f901d0d7e
Eshan-Sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-01-25 17:34:46 +0530

    structure and other files

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { Navbar, Footer, Services, Transactions, Welcome } from './components'
+import './App.css'
+
+const App= ()=>{
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='min-h-screen'>
+        <div className='gradient-bg-welcome'>
+          <Navbar/>
+          <Welcome/>
+        </div>
+        <Services/>
+        <Transactions/>
+        <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default App
