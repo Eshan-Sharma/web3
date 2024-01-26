f2b09007dc94ebb4936d97428940916294ea11e3
Eshan-Sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-01-26 17:23:09 +0530

    connecting wallet to the react fronent

--- client/context/TransactionContext.jsx
+++ client/context/TransactionContext.jsx
@@ -6,6 +6,7 @@ export const TransactionContext = React.createContext();
 
 const {ethereum} = window;
 
+// eslint-disable-next-line no-unused-vars
 const getEthereumContract = ()=>{
     const provider = new ethers.provider.Web3Provider(ethereum);
     const signer = provider.getSigner();
@@ -16,5 +17,39 @@ const getEthereumContract = ()=>{
 
 // eslint-disable-next-line react/prop-types
 export const TransactionProvider = ({children})=>{
-    return (<TransactionContext.Provider value={{}}>{children}</TransactionContext.Provider>)
+ 
+    const [currentAccount, setCurrentAccount] = useState('');
+
+    const checkIfWalletIsConnected = async () => {
+        try {
+            if(!ethereum) return alert("Please install metamask");
+    
+            const accounts = await ethereum.request({method: 'eth_accounts'});
+            console.log(accounts);
+            if(accounts.length){
+                setCurrentAccount(accounts[0]);
+            }
+            
+        } catch (error) {
+            console.log(error);
+            throw new error("No ethereum object");
+        }
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    const connectWallet = async ()=>{
+        try {
+            if(!ethereum) return alert("Please install metamask");
+            const accounts = await ethereum.request({method:'eth_requestAccounts'});
+            setCurrentAccount(accounts[0]);
+        } catch (error) {
+            console.log(error);
+            throw new error("No ethereum object");
+        }
+    }
+    useEffect(()=>{
+        checkIfWalletIsConnected();
+    },[]);
+
+    return (<TransactionContext.Provider value={{connectWallet, currentAccount}}>{children}</TransactionContext.Provider>)
 }
